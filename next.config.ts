d57b1354a678aee16122a84ae94e9a575d77b0c8
lylllllll-creator <lyl1158623541@163.com>
2026-04-22 00:46:34 +0800

    add static export config for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+import type { NextConfig } from "next";  
+  
+const nextConfig: NextConfig = {  
+  // 告诉 Next.js 导出适合 GitHub Pages 的静态网页  
+  output: "export",  
+  // 必须关闭图片优化，否则 GitHub Actions 会报错  
+  images: {  
+    unoptimized: true,  
+  },  
+};  
+  
+export default nextConfig;  
